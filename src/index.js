4a63d90bb635a648e604ed349e542d3c12f81f28
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-17 08:55:04 +0200

    avance miercoles 8:54

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
@@ -9,32 +11,15 @@ app.set('port', 3000);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
-const storage = multer.diskStorage({
-    destination: path.join(__dirname,'public/images'),
-    filename:(req, file, cb)=>{
-        cb(null, file.originalname);
-    }
-})
 
 //middlewares 
 //son codigos que se ejecutan antes de llegar a las rutas 
-app.use(multer({
-    storage,
-    dest:path.join(__dirname,'public/images')
-}).single('image'));
+
 
 //Routes
-app.get('/',(req, res)=> {
-    res.render('index');
-})
-
-app.post('/upload', (req,res) =>{
-    console.log(req.file);
-    res.send('uploaded')
-})
+app.use(require('./routes/index.routes'));
 //Start the server
 app.listen(app.get('port'), ()=>{
     console.log(`Server on port ${app.get('port')}`);
 })
 
-https://www.youtube.com/watch?v=AbJ-y2vZgBs tiempo 27:28
---
+++ src/routes/index.routes.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const path = require('path');
+const multer = require('multer');
+const router = Router();
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,'../public/images'),
+    filename:(req, file, cb)=>{
+        cb(null, file.originalname);
+    }
+})
+
+router.get('/',(req, res)=> {
+    res.render('index');
+})
+
+const upload = multer({
+    storage,
+    dest:path.join(__dirname,'public/images'),
+    limits: {fieldSize:1000000}
+}).single('image')
+
+router.post('/upload',upload ,(req,res) =>{
+    console.log(req.file);
+    res.send('uploaded')
+})
+
+module.exports = router;
